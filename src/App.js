23fa6f0607e1792ecdef30f9d2527aaf633f4d4c
tktharani <tktharani@gmail.com>
2024-06-02 20:12:35 +0530

    css styling done

--- src/App.js
+++ src/App.js
@@ -3,22 +3,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='currency-container'>
+      <div className='box'></div>
+      <div className='data'>
+        <h1>Currency Converter</h1>
+        <div className='input-container'>
+          <label htmlFor='amt'>Amount:</label>
+          <input type="number" id="amt"></input>
+        </div>
+        <div className='input-container'>
+          <label htmlFor='fromcurrency'>From Currency</label>
+          <select id="fromCurrency">
+              <option value="USD">USD-United States Dollar</option>
+              <option value="EUR">EUR-EUR</option>
+              <option value="GBP">GBP-British Pound Sterling</option>
+              <option value="JPY">JPY-Japanese Yen</option>
+              <option value="INR">INR-Indian Rupee</option>
+              <option value="CNY">CNY-Chinese Yuvan</option>
+          </select>
+        </div>
+        <div className='input-container'>
+          <label htmlFor='Tocurrency'>To Currency</label>
+          <select id="ToCurrency">
+              <option value="USD">USD-United States Dollar</option>
+              <option value="EUR">EUR-EUR</option>
+              <option value="GBP">GBP-British Pound Sterling</option>
+              <option value="JPY">JPY-Japanese Yen</option>
+              <option value="INR">INR-Indian Rupee</option>
+              <option value="CNY">CNY-Chinese Yuvan</option>
+          </select>
+        </div>
+        <div className='result'>
+          <p>1 INR is equai to 83.25USD</p> 
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
